6ce7c7c79844234712f9aba45034415261fd9435
Gustavo-Gon <gon.gustavo@outlook.com>
2025-06-29 16:05:01 -0700

    Initialized Amplify

--- Taplist/TaplistApp.swift
+++ Taplist/TaplistApp.swift
@@ -5,10 +5,20 @@
 //  Created by Gustavo Gonzalez on 6/29/25.
 //
 
+import Amplify
 import SwiftUI
 
 @main
 struct TaplistApp: App {
+    init() {
+        do {
+            try Amplify.configure()
+            print("Initialized Amplify");
+        } catch {
+            print("Could not initialize Amplify: \(error)")
+        }
+    }
+
     var body: some Scene {
         WindowGroup {
             NotesView()
